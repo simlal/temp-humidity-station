2bb40d24471d3aa3472fbec083e9e1f147edc362
Simon Lalonde <simlalonde@hotmail.com>
2024-05-13 20:34:44 -0400

    Add nix-shell env setup

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+    buildInputs = [
+        pkgs.marp-cli
+        pkgs.chromium
+    ];
+
+    shellHook = ''
+        echo "Entered nix-shell with marp-cli!"
+        echo "Building the docs with marp...\n"
+        ./build-docs.sh
+
+        # Check for successfull marp build
+        if [ $? -eq 1 ];then
+            echo Marp build unsuccessful. Try again.
+            exit 1
+        else
+            echo Marp build successful!
+            exit 0
+        fi       
+    '';
+}
