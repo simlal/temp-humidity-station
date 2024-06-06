0f6c36115e55db3fbab33de7edfb2cc53f2a27a0
Simon Lalonde <simlalonde@hotmail.com>
2024-06-06 14:21:40 -0400

    Create nix shell to compile gcc-arm

---
+++ picow_blink/shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+    buildInputs = [
+      pkgs.cmake
+      pkgs.gcc-arm-embedded
+    ];
+
+    shellHook = ''
+      echo Initialzing environment with Pi Pico toolchain...
+
+      echo Setting the 'PICO_SDK_PATH' to cloned 'pico-sdk' path...
+      PICO_SDK_PATH=${toString ../pico-sdk}
+      export PICO_SDK_PATH
+      
+      echo Setting the 'PICO_BOARD' to 'pico_w'...
+      PICO_BOARD=pico_w
+      export PICO_BOARD
+
+      echo Ready to build for Pi Pico W!
+    '';
+}
